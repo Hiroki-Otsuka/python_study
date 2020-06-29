e7c05a02eadbc5016723093e87431ddd28e98404
Hiroki Otsuka <>
2020-06-30 00:10:28 +0900

    Todays commit

---
+++ Function.py
@@ -0,0 +1,18 @@
+def say_hello(greeting):
+	print(greeting)
+
+# say_hello("hello world")
+
+# hello = say_hello
+# hello("good morning")
+
+def add(num01, num02):
+	return(num01 + num02)
+
+add_result = add(6,2)
+# print(add_result)
+
+def average(num01, num02, num03):
+	return (num01 + num02 + num03)/3
+
+print(average(9,4,2))
